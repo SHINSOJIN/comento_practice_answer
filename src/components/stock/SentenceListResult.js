47ddb75d2794b4f26d4498f3d0273fe2dc93c486
wooyeonwon <dndus567@naver.com>
2023-02-02 14:12:05 +0900

    [원우연/코멘토 2주차 과제완료]

--- src/components/stock/SentenceListResult.js
+++ src/components/stock/SentenceListResult.js
@@ -1,81 +1,35 @@
 import sentence from "../common/sentenceList.js";
 
-
 const SentenceResult = () => {
   return (
     <>
-      <div
-        class="sentenceReport2"
-        style={{ backgroundColor: "rgba(101, 119, 152, 0.15)" }}
-      >
-        <p class="sentence">{sentence[0].sentence}</p>
-        <p class="percent">
-          <span>{sentence[0].condition[0].stock}&nbsp;&nbsp;&nbsp;</span>
-          <span
-            style={{
-              color: "rgb(101, 119, 152)",
-              fontWeight: "bold",
-              opacity: "1",
-            }}
-          >
-            {sentence[0].condition[0].score.toFixed(2) * 100}%
-          </span>
-        </p>
-      </div>
-      <div
-        class="sentenceReport2"
-        style={{ backgroundColor: "rgba(98, 218, 171, 0.15)" }}
-      >
-        <p class="sentence">{sentence[1].sentence}</p>
-        <p class="percent">
-          <span>{sentence[1].condition[0].stock}&nbsp;&nbsp;&nbsp;</span>
-          <span
-            style={{
-              color: "rgb(98, 218, 171)",
-              fontWeight: "bold",
-              opacity: "1",
-            }}
-          >
-            {sentence[1].condition[0].score.toFixed(2) * 100}%
-          </span>
-        </p>
-      </div>
-      <div
-        class="sentenceReport2"
-        style={{ backgroundColor: "rgba(99, 149, 249, 0.15)" }}
-      >
-        <p class="sentence">{sentence[2].sentence}</p>
-        <p class="percent">
-          <span>{sentence[2].condition[0].stock}&nbsp;&nbsp;&nbsp;</span>
-          <span
-            style={{
-              color: "rgb(99, 149, 249)",
-              fontWeight: "bold",
-              opacity: "1",
-            }}
-          >
-            {sentence[2].condition[0].score.toFixed(2) * 100}%
-          </span>
-        </p>
-      </div>
-      <div
-        class="sentenceReport2"
-        style={{ backgroundColor: "rgb(245, 245, 245)" }}
-      >
-        <p class="sentence">{sentence[3].sentence}</p>
-        <p class="percent">
-          <span>없음</span>
-        </p>
-      </div>
-      <div
-        class="sentenceReport2"
-        style={{ backgroundColor: "rgb(245, 245, 245)" }}
-      >
-        <p class="sentence">{sentence[4].sentence}</p>
-        <p class="percent">
-          <span>없음</span>
-        </p>
-      </div>
+      {sentence.map((sent, index) => (
+        <div
+          key={index}
+          className="sentenceReport2"
+          style={{ backgroundColor: `rgba(${sent.color}, 0.15)` }}
+        >
+          <p className="sentence">{sent.sentence}</p>
+          <p className="percent">
+            {sent.condition.length ? (
+              <>
+                <span>{sent.condition[0].stock}&nbsp;&nbsp;&nbsp;</span>
+                <span
+                  style={{
+                    color: `rgb(${sent.color})`,
+                    fontWeight: "bold",
+                    opacity: "1",
+                  }}
+                >
+                  {sent.condition[0].score.toFixed(2) * 100}%
+                </span>
+              </>
+            ) : (
+              <span>없음</span>
+            )}
+          </p>
+        </div>
+      ))}
     </>
   );
 };
