16fe05a94a7a172ad8e08ddb0d9679f54dfc3869
sojin <sojin@2digit.io>
2023-02-09 15:01:02 +0900

    [2023-02-09] 3주차 과제 답

--- src/components/stock/Demo.js
+++ src/components/stock/Demo.js
@@ -1,14 +1,39 @@
+import {useState, useEffect} from 'react';
+import axios from 'axios'
 import NewsInsert from "./NewsInsert";
 import Result from "./Result";
+import sampleNews  from "../../assets/datas/sampleNews";
 
 //https://charts.ant.design/en/examples/pie/basic#basic
 const Demo = () => {
+    const [loading, setLoading] = useState(false); // 로딩스피너
+    const [keywordIdx, setKeywordIdx] = useState(0); //뉴스 키워드 인덱스
+    const [result, setResult] = useState({}); // 종목명 API 리턴값 저장
+    const [stockList, setStockList] = useState([]) // 추출된 전체 종목명 배열
+
+
+    const analysisClick = (e) => {
+        e.preventDefault()
+        setLoading(true)
+
+        const url = 'https://d9390710-b9c8-490b-8005-e11d0772b58c.mock.pstmn.io'
+        axios.get(`${url}/stock/news?idx=${keywordIdx}`)
+            .then(res => {
+                const list = res.data.answer.sort((x, y) => y.score - x.score).map(el => el.stock)
+                setResult(res.data)
+                setStockList(list)
+            })
+            .catch(err => console.error(err))
+
+    };
+
+
     return (
         <>
             <div style={{backgroundColor: 'rgb(255, 255, 255)'}}>
                 <div className="demoContainer ">
-                    <NewsInsert/>
-                    <Result/>
+                    <NewsInsert keywordIdx={keywordIdx} setKeywordIdx={setKeywordIdx} analysisClick={analysisClick}/>
+                    <Result result={result} stockList={stockList} loading={loading}/>
                 </div>
             </div>
         </>
--- src/components/stock/NewsInsert.js
+++ src/components/stock/NewsInsert.js
@@ -1,4 +1,14 @@
-const NewsInsert = () => {
+import {useState, useEffect} from 'react';
+import sampleNews  from "../../assets/datas/sampleNews";
+
+const NewsInsert = ({keywordIdx, setKeywordIdx, analysisClick}) => {
+    const [title, setTitle] = useState('')
+    const [context, setContext] = useState('')
+
+    useEffect(()=> {
+        setTitle(sampleNews[keywordIdx].query)
+        setContext(sampleNews[keywordIdx].context)
+    }, [keywordIdx])
 
     return (
         <>
@@ -8,29 +18,26 @@ const NewsInsert = () => {
                     <div className="keywordBtnList">
                         <div></div>
                         <span><strong>샘플 뉴스</strong></span>
-                        <span className="keywordBtn ">카카오</span>
-                        <span className="keywordBtn click ">화장품</span>
-                        <span className="keywordBtn ">삼성</span>
-                        <span className="keywordBtn ">스마트폰</span>
+                        <span className={`keywordBtn ${keywordIdx === 0 ? 'click' : ''}`} onClick={()=>setKeywordIdx(0)}>카카오</span>
+                        <span className={`keywordBtn ${keywordIdx === 1 ? 'click' : ''}`} onClick={()=>setKeywordIdx(1)}>화장품</span>
+                        <span className={`keywordBtn ${keywordIdx === 2 ? 'click' : ''}`} onClick={()=>setKeywordIdx(2)}>삼성</span>
+                        <span className={`keywordBtn ${keywordIdx === 3 ? 'click' : ''}`} onClick={()=>setKeywordIdx(3)}>스마트폰</span>
                     </div>
                 </div>
                 <div className="inputWrapper">
                     <div>
                         <div>
                             <input aria-invalid="false" id="inputTitle" placeholder="뉴스 제목" type="text"
-                                   value="&quot;한솔케미칼, 올해·내년 영업이익 전망치 하향에 목표가↓&quot;-SK"/>
+                                   defaultValue={title}/>
                         </div>
                     </div>
                     <div>
                         <div>
-                                <textarea
-                                    rows="12" aria-invalid="false" id="inputContext" placeholder="뉴스 본문"
-                                    style={{height: '276px'}}>SK증권은 한솔케미칼에 대해 올해와 내년 연결 영업이익 전망치 하향이 예상된다며 투자의견은 '매수'를 유지했으나 목표주가는 28만원으로 15% 하향했다.한동희 SK증권 연구원은 24일 "3분기 실적 부진과 내년 업황 둔화를 감안해 한솔케미칼의 올해와 내년 연결 영업이익 전망치를 각각 12%, 13% 하향 조정한다"고 말했다. 앞서 한솔케미칼의 3분기 연결 실적은 매출 2160억원, 영업이익 457억원으로 시장컨센서스를 각각 6%, 16% 하회했다. 당초 3분기 TV 등 성수기 진입 효과에 따른 퀀텀닷(QD) 소재 회복을 예상했으나 글로벌 경기 위축에 따른 전방 재고 조정으로 QD 소재 및 테이팩스의 스마트폰향 판매 부진, 천연가스 및 유가 등 원재료 가격 상승 영향으로 과산화수소의 수익성이 하락한 영향으로 추정된다.올해 4분기 연결 실적은 매출 2355억원, 영업이익 353억원으로 예상된다. QD 소재는 전방 재고조정 일단락 효과에 따른 회복, 과산화수소는 원재료 가격 상승분의 판가 전가로 수익성이 회복되기 시작할 것으로 전망된다.
-                                </textarea>
+                            <textarea rows="12" aria-invalid="false" id="inputContext" placeholder="뉴스 본문" style={{height: '276px'}} defaultValue={context}/>
                         </div>
                     </div>
                 </div>
-                <button className="analysisBtn" tabIndex="0" type="button">종목명 추출하기</button>
+                <button className="analysisBtn" tabIndex="0" type="button" onClick={analysisClick}>종목명 추출하기</button>
             </div>
 
         </>
